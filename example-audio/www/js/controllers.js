7b04e4f225a3cae160ba947076c5970cc0041797
Ariel Faur <ariel.faur@gmail.com>
2016-07-27 12:29:40 -0300

    test example with brett's audio

--- example-audio/www/js/controllers.js
+++ example-audio/www/js/controllers.js
@@ -1,26 +1,20 @@
 angular.module('starter.controllers', [])
 
 .controller('MusicCtrl', ['$scope', 'MediaManager', function($scope, MediaManager) {
-    var urlprefix = '/android_asset/www/audio/';
-
     $scope.dynamicTrack = {};
 
     $scope.tracks = [
         {
-            url: urlprefix + '03 - Land Of Confusion.mp3',
-            artist: 'Genesis',
-            title: 'Land of Confusion'
-        },
-        {
-            url: urlprefix + '02 - Tonight. Tonight. Tonight.mp3',
-            artist: 'Genesis',
-            title: 'Tonight. Tonight. Tonight'
+            url: 'http://www.oh2o.org/wp-content/uploads/2016/07/ss2016_07_10.mp3',
+            artist: 'Some artist',
+            title: 'Some audio title'
         }
     ];
 
     $scope.stopPlayback = function() {
         MediaManager.stop();
     };
+    
     $scope.playTrack = function(index) {
         $scope.dynamicTrack = $scope.tracks[index];
 
